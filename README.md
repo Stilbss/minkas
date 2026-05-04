0ea3171b2840cecac8f197283ae2a80f261d616d
stilbss <atrurs.javnosans@gmail.com>
2026-05-04 21:51:25 +0200

    12. nodarbibas beigas

---
+++ README.md
@@ -0,0 +1,13 @@
+# Projekts "Minka"
+
+*Mūsu pirmais objektorientētais projekts.Projekts tiks versonēts ar git un izvietots GitHub*
+
+![Minka foto](https://www.google.com/url?sa=t&source=web&rct=j&url=https%3A%2F%2Fpngtree.com%2Fso%2Fsurprised-cat&ved=0CBYQjRxqFwoTCOjG2o2roJQDFQAAAAAdAAAAABAo&opi=89978449)
+
+**Darāmo darbu saraksts:**
+-[] izveidot klasi "Minka"
+-[]Izveidot klasei "MinkuTante"
+-[]Izveidot klasi "DarbsArFailu"
+-[]Versionet visu izstrades procesu
+-[]Izvietot projektu GitHub
+-[]Sagatavot projekta .jar failu
--- src/uzd1/Minka.java
+++ src/uzd1/Minka.java
@@ -1,5 +1,39 @@
 package uzd1;
 
-public class Minka {
+import java.time.Year;
+import java.util.Random;
 
+import javax.swing.ImageIcon;
+
+public class Minka {
+//Atributi
+	String vards, skirne, spalvasKrasa, saimnieks;
+	int vecums, medijumuSk, izsalkums;
+	boolean siksnina;
+	ImageIcon bilde;
+	Random rand = new Random();
+	
+	//Konstruktors
+	
+	public Minka(String vards,String skirne,String spalvasKrasa,String saimnieks, int dzGads, boolean siksnina, String cels) {
+		this.vards = vards;
+		this.skirne = skirne;
+		this.spalvasKrasa = spalvasKrasa;
+		this.saimnieks = saimnieks;
+		vecums = Year.now().getValue()- dzGads;
+		medijumuSk = 0;
+		izsalkums = kungis();
+		this.siksnina = siksnina;
+		bilde = iestatitBildi(cels);
+	}
+	
+	//metodes 
+	public int kungis() {
+		return rand.nextInt(5)+1;
+	}
+	
+	public ImageIcon iestatitBildi() {
+		
+		return null;
+	}
 }
